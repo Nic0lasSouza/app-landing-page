9a55f254c4d191089eea4043accb07dcd8f1505e
Nicolas Souza <nicolasdsmdsouza2004@gmail.com>
2023-02-11 19:20:15 -0300

    atualizado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import logo from '../images/logo.png';
+import { useState } from 'react';
 
 function Navbar() {
+
+  const [nav, setnav] = useState(false);
+  const changeBackground = () =>{
+    if(window.scrollY >= 50){
+      setnav(true);
+    }
+    else{
+      setnav(false);
+    }
+  }
+  window.addEventListener('scroll', changeBackground);
   return (
-    <nav className={}>
+    <nav className={nav ? 'nav active' : 'nav'}>
         <a href='#' className='logo'>
             <img src={logo} alt=''/>
         </a>
@@ -12,7 +24,11 @@ function Navbar() {
             <span className='nav-icon'></span>
         </label>
         <ul className='menu'>
-            <li></li>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>Features</a></li>
+            <li><a href='#'>About is</a></li>
+            <li><a href='#'>Ui SS</a></li>
+            <li><a href='#'>Download</a></li>
         </ul>
     </nav>
   )

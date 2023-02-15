2b16644d3ad8544983e5b93e4a72d3553bd5f4f0
Nicolas Souza <nicolasdsmdsouza2004@gmail.com>
2023-02-14 21:56:34 -0300

    modificações gerais

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from './components/Header';
 import Feature from "./components/Feature";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Feature/>
+      <About/>
     </div>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-function About() {
+function About(props) {
   return (
     <div id='about'>
-        <div className=''>
-            <img src={} alt=''/>
+        <div className='about-image'>
+            <img src={props.image} alt=''/>
+        </div>
+        <div className='about-text'>
+          <h2>{props.title}</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis, eros et auctor tincidunt, lectus ipsum tristique nunc, tincidunt porttitor nulla arcu tincidunt odio. Curabitur ex libero, finibus in turpis eget, porttitor lacinia lectus. Nunc.</p>
+          <button>{props.button}</button>
         </div>
     </div>
   )

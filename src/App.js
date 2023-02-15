9cb4a4c24a11db8eceb7e944e6bd9239b0dba94e
Nicolas <nicolasdsmdsouza2004@gmail.com>
2023-02-15 10:44:57 -0300

    contruindo css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import Header from './components/Header';
 import Feature from "./components/Feature";
 import About from "./components/About";
+import Presentation from "./components/Presentation";
+import aboutimages from "./images/about.png";
+import aboutimages1 from "./images/download.png";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Feature/>
-      <About/>
+      <About image={aboutimages} title='Comes With All You Need For Daily Life' button='Get The App'/>
+      <Presentation/>
+      <About image={aboutimages1} title='Download And Get The APP Now' button='Download'/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div id='contact'>
+            <h3> Spend Me Email</h3>
+        <div className='contact-input'>
+            <input type='email' placeholder='exemple@gmail.com'></input>
+            <a href='#'>Contact</a>
+        </div>
+    </div>
+  )
+}
+// 22:42
+export default Contact;
